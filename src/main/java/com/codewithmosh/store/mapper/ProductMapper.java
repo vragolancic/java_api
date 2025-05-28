2a26e47ba0ff0d8b5f5cecaf7716bf5a981223f5
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2025-05-28 09:14:33 +0200

    Produc dodat i odradjena vezna sa product categoryId

---
+++ src/main/java/com/codewithmosh/store/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.mapper;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mapping(target = "categoryId", source = "category.id")
+    ProductDto toDto(Product product);
+}
