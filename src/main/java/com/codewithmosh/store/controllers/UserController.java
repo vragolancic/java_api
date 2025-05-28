cfde52ac21cb0e9032eb7279a8ad18dcfcc95ac7
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2025-05-28 23:08:21 +0200

    Kreiranje Cart i Cart Item

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -49,10 +49,16 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
+    public ResponseEntity<?> registerUser(
             @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder
     ) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email is already registered.")
+            );
+        }
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
