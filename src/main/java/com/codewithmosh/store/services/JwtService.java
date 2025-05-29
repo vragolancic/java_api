adad0cf34107bd2d731d256e087eceb67c08259e
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2025-05-30 00:19:10 +0200

    config Jwt tokena i neko cudo u pom za prebacivanje kljuca van app

---
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -0,0 +1,26 @@
+package com.codewithmosh.store.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+    public String generateToken(String username) {
+        final long tokenExpiration = 86400; // jedan dan u sekundama
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+}
